6279bd19204d3787f9a4bb15f47fa9c53a410a5a
sydleither <sydleither@protonmail.com>
2026-05-07 17:53:19 -0400

    update running exponential growth rate experiments

--- README.md
+++ README.md
@@ -41,39 +41,28 @@ With the post-cellprofiler data, replicate this file structure:
                 └── segmentation_results_well_{...}_locations_{...}.csv
 ```
 
-## Find ideal exponential growth window stratedgy
-```
-python3 run_game_assay.py -dir data/experimental
-python3 exponential_growth_window.py -w per_cell
-python3 exponential_growth_window.py -w per_well
-python3 exponential_growth_window.py -w per_exp
-python3 exponential_growth_window.py -w none
-python3 exponential_growth_window.py -p 1
-```
-
 ## Run game assay, ODE models, and plot classification accuracy:
-### On frequency-dependent ODE data
+### On perfect ODE data
 ```
 python3 create_ode_data.py -dir data/ode_egt -model replicator
 bash data/ode_egt/fit_models.sh
-python3 compare_estimations.py -dir data/ode_egt
+python3 analyze_synthetic.py -dir data/ode_egt
 ```
-### On density-dependent ODE data
 ```
 python3 create_ode_data.py -dir data/ode_lv -model "lotka-volterra"
 bash data/ode_lv/fit_models.sh
-python3 compare_estimations.py -dir data/ode_lv
+python3 analyze_synthetic.py -dir data/ode_lv
 ```
 ### On noisy ODE data
 ```
 python3 create_ode_data.py -dir data/ode_egt_noisy -model replicator -noise 0.2
 bash data/ode_egt_noisy/fit_models.sh
-python3 compare_estimations.py -dir data/ode_egt_noisy
+python3 analyze_synthetic.py -dir data/ode_egt_noisy
 ```
 ```
 python3 create_ode_data.py -dir data/ode_lv_noisy -model "lotka-volterra" -noise 0.2
 bash data/ode_lv_noisy/fit_models.sh
-python3 compare_estimations.py -dir data/ode_lv_noisy
+python3 analyze_synthetic.py -dir data/ode_lv_noisy
 ```
 ### On spatial data
 ```
@@ -81,19 +70,33 @@ python3 create_abm_data.py -dir data/abm_strong -m 2 -n 2
 bash data/abm_strong/raw/run0.sh
 python3 format_abm_data.py -dir data/abm_strong
 bash data/abm_strong/fit_models.sh
-python3 compare_estimations.py -dir data/abm_strong/formatted
+python3 analyze_synthetic.py -dir data/abm_strong/formatted
 ```
 ```
 python3 create_abm_data.py -dir data/abm_medium -m 4 -n 4
 bash data/abm_medium/raw/run0.sh
 python3 format_abm_data.py -dir data/abm_medium
 bash data/abm_medium/fit_models.sh
-python3 compare_estimations.py -dir data/abm_medium/formatted
+python3 analyze_synthetic.py -dir data/abm_medium/formatted
 ```
 ```
 python3 create_abm_data.py -dir data/abm_weak -m 6 -n 6
 bash data/abm_weak/raw/run0.sh
 python3 format_abm_data.py -dir data/abm_weak
 bash data/abm_weak/fit_models.sh
-python3 compare_estimations.py -dir data/abm_weak/formatted
+python3 analyze_synthetic.py -dir data/abm_weak/formatted
+```
+### On experimental data
+'''
+python3 run_game_assay.py -dir data/experimental
+python3 fit_ode.py -dir data/experimental -model replicator
+python3 fit_ode.py -dir data/experimental -model "lotka-volterra"
+python3 analyze_experimental.py -dir data/experimental
+'''
+
+## Test different exponential growth windows
+```
+python3 exponential_growth_window.py -in data/{data directory name}
+bash data/{data directory name}_gr/exponential_growth_windows.sh
+python3 exponential_growth_window.py -in data/{data directory name} -p 1
 ```
--- exponential_growth_window.py
+++ exponential_growth_window.py
@@ -1,20 +1,12 @@
 import argparse
 import os
 
-import matplotlib.colors as mcolors
-import matplotlib.pyplot as plt
-import numpy as np
 import pandas as pd
-from scipy.stats import mannwhitneyu
-import seaborn as sns
-from sklearn.metrics import accuracy_score
 
-from compare_fits import (
+from utils import (
     format_for_plotting,
     get_fit_df,
     label_qualitative_dynamics,
-    plot_errors,
-    plot_qualitative,
 )
 from fit_ode import fit
 from game_assay.game_analysis import calculate_counts, calculate_growth_rates, calculate_payoffs
@@ -23,229 +15,7 @@ from game_assay.game_analysis_utils import (
     optimize_growth_rate_window_per_well,
 )
 from run_game_assay import plot_fits, plot_freqdepend_fit
-from utils import get_cell_types, get_colors, label_data_type
-
-
-def plot_agreement(data_dir, df, data_type):
-    def heatmap(df_model_agr, model):
-        df_model_agr = df_model_agr.pivot(
-            index="Exponential Growth Window Strategy 1",
-            columns="Exponential Growth Window Strategy 2",
-            values="Agreement",
-        )
-        fig, ax = plt.subplots(figsize=(6, 6))
-        sns.heatmap(df_model_agr, annot=True, vmin=0, vmax=1, ax=ax)
-        ax.set_title(f"Agreement Between Exponential Growth Window Strategys for {model}")
-        fig.patch.set_alpha(0.0)
-        fig.tight_layout()
-        fig.savefig(f"{data_dir}/agreement_{model}.png", bbox_inches="tight", dpi=200)
-        plt.close()
-
-    df_agr = []
-    for model in df["Model"].unique():
-        df_model = df[df["Model"] == model]
-        df_q = df_model[
-            ["Exponential Growth Window Strategy", "Experiment", "Dynamic"]
-        ].drop_duplicates()
-        df_q = df_q.sort_values(by=["Exponential Growth Window Strategy", "Experiment"])
-        gr_windows = df_model["Exponential Growth Window Strategy"].unique()
-
-        agreements = []
-        for i in range(len(gr_windows)):
-            for j in range(i + 1, len(gr_windows)):
-                acc = accuracy_score(
-                    df_q[df_q["Exponential Growth Window Strategy"] == gr_windows[i]]["Dynamic"],
-                    df_q[df_q["Exponential Growth Window Strategy"] == gr_windows[j]]["Dynamic"],
-                )
-                agreements.append(
-                    {
-                        "Exponential Growth Window Strategy 1": gr_windows[i],
-                        "Exponential Growth Window Strategy 2": gr_windows[j],
-                        "Agreement": acc,
-                    }
-                )
-
-        df_model_agr = pd.DataFrame(agreements)
-        df_model_agr2 = df_model_agr.copy()
-        df_model_agr2["temp"] = df_model_agr2["Exponential Growth Window Strategy 1"]
-        df_model_agr2["Exponential Growth Window Strategy 1"] = df_model_agr2[
-            "Exponential Growth Window Strategy 2"
-        ]
-        df_model_agr2["Exponential Growth Window Strategy 2"] = df_model_agr2["temp"]
-        df_model_agr = pd.concat([df_model_agr, df_model_agr2])
-        heatmap(df_model_agr, model)
-        df_model_agr["Model"] = model
-        df_agr.append(df_model_agr)
-
-    df_agr = pd.concat(df_agr, ignore_index=True)
-    if "Ground Truth" in df_agr["Exponential Growth Window Strategy 1"].unique():
-        df_agr = df_agr.sort_values(by="Model")
-        df_agr["Exponential Growth Window Strategy"] = df_agr[
-            "Exponential Growth Window Strategy 1"
-        ]
-        df_agr = df_agr[df_agr["Exponential Growth Window Strategy 2"] == "Ground Truth"]
-        fig, ax = plt.subplots(figsize=(5, 4))
-        sns.barplot(
-            df_agr, x="Model", y="Agreement", hue="Exponential Growth Window Strategy", ax=ax
-        )
-        sns.move_legend(ax, "lower center", bbox_to_anchor=(0.5, 1))
-        ax.tick_params("x", rotation=45)
-        ax.set(ylabel="Qualitative Interaction\nClassification Accuracy")
-        fig.suptitle(f"Exponential Growth Window Accuracy\non {data_type} data", y=1.33)
-        fig.patch.set_alpha(0.0)
-        fig.tight_layout()
-        fig.savefig(f"{data_dir}/accuracy_{data_type}.png", bbox_inches="tight", dpi=200)
-        plt.close()
-
-
-def plot_parameter_ranges(data_dir, df, data_type):
-    df_param = pd.melt(
-        df,
-        id_vars=["Model", "Exponential Growth Window Strategy", "Experiment"],
-        value_vars=[x for x in df.columns if x[0] == "$"],
-        var_name="Parameter",
-        value_name="Value",
-    )
-    df_param = df_param.reset_index(drop=True).dropna().drop_duplicates()
-    df_param.loc[df_param["Parameter"].str.contains("A"), "Model"] = (
-        "Lotka-Volterra Interaction Matrix"
-    )
-    df_param.loc[df_param["Parameter"].str.contains("r"), "Model"] = (
-        "Lotka-Volterra Intrinsic Growths"
-    )
-    df_param = df_param.sort_values(by=["Model", "Experiment", "Parameter"], ascending=False)
-
-    facet_grid = sns.catplot(
-        df_param,
-        col="Model",
-        x="Parameter",
-        y="Value",
-        hue="Exponential Growth Window Strategy",
-        kind="box",
-        sharex=False,
-        sharey=False,
-        height=4,
-        aspect=1,
-    )
-    facet_grid.set_titles("{col_name}")
-    facet_grid.figure.suptitle(f"Parameter Ranges of Models Fit on {data_type} Data")
-    facet_grid.figure.patch.set_alpha(0.0)
-    facet_grid.tight_layout()
-    facet_grid.savefig(f"{data_dir}/parameter_range.png", bbox_inches="tight", dpi=200)
-    plt.close()
-
-    for window in df["Exponential Growth Window Strategy"].unique():
-        facet_grid = sns.FacetGrid(
-            df_param[df_param["Exponential Growth Window Strategy"] == window],
-            col="Model",
-            sharex=False,
-            sharey=False,
-            height=4,
-            aspect=1,
-        )
-        facet_grid.map_dataframe(sns.boxplot, x="Parameter", y="Value")
-        facet_grid.set_titles("{col_name}")
-        facet_grid.figure.suptitle(
-            f"Parameter Ranges of Models Fit on {data_type} Data under {window} Window Strategy"
-        )
-        facet_grid.figure.patch.set_alpha(0.0)
-        facet_grid.tight_layout()
-        facet_grid.savefig(f"{data_dir}/parameter_range_{window}.png", bbox_inches="tight", dpi=200)
-        plt.close()
-
-
-def plot_dynamics(save_loc, df, data_type):
-    df_dynamic = (
-        df[["Exponential Growth Window Strategy", "Model", "Experiment", "Dynamic"]]
-        .drop_duplicates()
-        .groupby(["Exponential Growth Window Strategy", "Model", "Dynamic"])
-        .count()
-        .reset_index()
-    )
-    dynamics = list(get_colors().keys())
-    n_colors = len(dynamics)
-    palette_list = [get_colors()[d] for d in dynamics]
-    custom_cmap = mcolors.ListedColormap(palette_list)
-    dynamics_to_int = {s: i for i, s in enumerate(dynamics)}
-
-    facet_grid = sns.displot(
-        df_dynamic,
-        col="Model",
-        x="Exponential Growth Window Strategy",
-        weights="Experiment",
-        hue="Dynamic",
-        hue_order=dynamics,
-        palette=get_colors(),
-        kind="hist",
-        multiple="stack",
-        height=4,
-        aspect=1,
-    )
-    facet_grid.add_legend()
-    facet_grid.set_titles("{col_name}")
-    facet_grid.set_xticklabels(rotation=45)
-    facet_grid.figure.suptitle(f"Classified Qualitative Interactions of {data_type} Data")
-    sns.move_legend(facet_grid, loc="center right", bbox_to_anchor=(1.1, 0))
-    facet_grid.figure.patch.set_alpha(0.0)
-    facet_grid.tight_layout()
-    facet_grid.savefig(f"{save_loc}/window_dynamics.png", bbox_inches="tight", dpi=200)
-    plt.close()
-
-    df = df[
-        ["Experiment", "Model", "Exponential Growth Window Strategy", "Dynamic"]
-    ].drop_duplicates()
-    models = sorted(df["Model"].unique())
-    df["Experiment"] = df["Experiment"].map({s: i for i, s in enumerate(df["Experiment"].unique())})
-    fig, ax = plt.subplots(1, len(models), figsize=(4 * len(models), 4))
-    for i, model in enumerate(models):
-        df_model = (
-            df[df["Model"] == model]
-            .pivot(
-                index="Experiment", columns="Exponential Growth Window Strategy", values="Dynamic"
-            )
-            .replace(dynamics_to_int)
-        )
-        sns.heatmap(
-            df_model,
-            cmap=custom_cmap,
-            ax=ax[i],
-            vmin=-0.5,
-            vmax=n_colors - 0.5,
-            cbar=(i == len(models) - 1),
-            cbar_kws={"ticks": range(n_colors), "label": "Dynamics"}
-            if i == len(models) - 1
-            else None,
-        )
-        ax[i].set_title(model)
-        ax[i].tick_params("x", rotation=45)
-    last_cbar = ax[-1].collections[0].colorbar
-    if last_cbar:
-        last_cbar.set_ticklabels(dynamics)
-        last_cbar.ax.invert_yaxis()
-    fig.suptitle("Experiment Classifications across Models and Windows")
-    fig.patch.set_alpha(0.0)
-    fig.tight_layout()
-    fig.savefig(f"{save_loc}/window_dynamics_matched.png", bbox_inches="tight", dpi=200)
-    plt.close()
-
-
-def print_stats(df):
-    # Parameter variance
-    df_param = pd.melt(
-        df,
-        id_vars=["Model", "Exponential Growth Window Strategy", "Experiment"],
-        value_vars=[x for x in df.columns if x[0] == "$"],
-        var_name="Parameter",
-        value_name="Value",
-    )
-    df_param = df_param.reset_index(drop=True).dropna().drop_duplicates()
-    df_param = df_param.drop(["Experiment"], axis=1)
-    variances = (
-        df_param.groupby(["Parameter", "Model", "Exponential Growth Window Strategy"])["Value"]
-        .std()
-        .reset_index()
-    )
-    print(variances.loc[variances.groupby(["Model", "Parameter"])["Value"].idxmin()])
+from utils import get_cell_types
 
 
 def get_ground_truth(in_data_dir, df):
@@ -257,33 +27,36 @@ def get_ground_truth(in_data_dir, df):
     return pd.concat([df, df_gt])
 
 
-def get_growth_rates(data_dir, in_dir):
+def get_growth_rates(in_dir, out_dir):
     # Read in all the data files
     df = []
-    for gr_window in os.listdir(data_dir):
-        if os.path.isfile(f"{data_dir}/{gr_window}"):
+    for window in os.listdir(out_dir):
+        if os.path.isfile(f"{out_dir}/{window}"):
             continue
-        df_window = get_fit_df(f"{data_dir}/{gr_window}")
-        df_window["Growth Rate Window"] = gr_window
-        df.append(df_window)
+        for rep in os.listdir(f"{out_dir}/{window}"):
+            if os.path.isfile(f"{out_dir}/{window}/{rep}"):
+                continue
+            df_window = get_fit_df(f"{out_dir}/{window}/{rep}")
+            df_window["Replicate"] = rep
+            df_window["Growth Rate Window"] = window
+            df.append(df_window)
     df = pd.concat(df)
 
     # Final formatting and return
     df = get_ground_truth(in_dir, df)
-    df = label_qualitative_dynamics(df, ["Growth Rate Window", "Model", "Experiment"])
+    df = label_qualitative_dynamics(df, ["Growth Rate Window", "Replicate", "Model", "Experiment"])
     df = format_for_plotting(df)
     return df.reset_index()
 
 
-def save_growth_rate(in_data_dir, out_data_dir, exp_name, window):
-    save_loc = f"{out_data_dir}/{window}/{exp_name}/images"
-    if not os.path.exists(save_loc):
-        os.makedirs(save_loc)
-
+def save_growth_rate(in_dir, out_dir, replicate, exp_name, window):
+    # Set variables
+    save_loc = f"{out_dir}/{window}/{replicate}/{exp_name}/images"
     sensitive_type, resistant_type = get_cell_types(exp_name)
     cell_types = [sensitive_type, resistant_type]
-    counts_df = calculate_counts(in_data_dir, exp_name)
+    counts_df = calculate_counts(f"{in_dir}/{replicate}", exp_name)
 
+    # Set exponential growth window
     gr_window = None
     if window == "none":
         gr_window = (counts_df["Time"].min(), counts_df["Time"].max())
@@ -292,8 +65,9 @@ def save_growth_rate(in_data_dir, out_data_dir, exp_name, window):
     elif window == "per_exp":
         counts_df = optimize_growth_rate_window_per_exp(counts_df)
 
+    # Calculate growth rates and plot fits
     growth_rate_df = calculate_growth_rates(
-        f"{out_data_dir}/{window}",
+        f"{out_dir}/{window}/{replicate}",
         exp_name,
         counts_df,
         growth_rate_window=gr_window,
@@ -302,8 +76,9 @@ def save_growth_rate(in_data_dir, out_data_dir, exp_name, window):
     cell_colors = {sensitive_type: "#4C956C", resistant_type: "#EF7C8E"}
     plot_fits(save_loc, exp_name, counts_df, growth_rate_df, cell_types, cell_colors)
 
+    # Plot frequency-dependent fits
     payoff_df = calculate_payoffs(
-        f"{out_data_dir}/{window}",
+        f"{out_dir}/{window}/{replicate}",
         exp_name,
         growth_rate_df,
         cell_types,
@@ -311,8 +86,9 @@ def save_growth_rate(in_data_dir, out_data_dir, exp_name, window):
     )
     plot_freqdepend_fit(save_loc, exp_name, growth_rate_df, payoff_df, cell_colors, cell_types)
 
+    # Fit ODEs
     fit(
-        f"{out_data_dir}/{window}",
+        f"{out_dir}/{window}/{replicate}",
         exp_name,
         "replicator",
         counts_df,
@@ -320,7 +96,7 @@ def save_growth_rate(in_data_dir, out_data_dir, exp_name, window):
         save_figs=True,
     )
     fit(
-        f"{out_data_dir}/{window}",
+        f"{out_dir}/{window}/{replicate}",
         exp_name,
         "lotka-volterra",
         counts_df,
@@ -329,82 +105,46 @@ def save_growth_rate(in_data_dir, out_data_dir, exp_name, window):
     )
 
 
+def write_run_script(in_dir, out_dir, run_cmd):
+    os.makedirs(out_dir)
+    with open(f"{out_dir}/exponential_growth_windows.sh", "w") as f:
+        for window in ["none", "per_exp", "per_well", "per_cell"]:
+            for rep in os.listdir(in_dir):
+                if os.path.isfile(f"{in_dir}/{rep}"):
+                    continue
+                for exp in os.listdir(f"{in_dir}/{rep}"):
+                    if os.path.isfile(f"{in_dir}/{rep}/{exp}") or exp == "layout_files":
+                        continue
+                    os.makedirs(f"{out_dir}/{window}/{rep}/{exp}/images")
+                    arg_str = f"-in {in_dir} -rep {rep} -exp {exp} -w {window}"
+                    f.write(f"{run_cmd} exponential_growth_window.py {arg_str}\n")
+
+
 def main():
     # Read in arguments
     parser = argparse.ArgumentParser()
-    parser.add_argument("-dir", "--data_dir", type=str, default="data")
-    parser.add_argument("-in", "--in_dir", type=str, default="experimental")
-    parser.add_argument("-out", "--out_dir", type=str, default="gr_experimental")
+    parser.add_argument("-in", "--in_dir", type=str)
+    parser.add_argument("-run", "--run_cmd", type=str, default="python3")
+    parser.add_argument("-rep", "--replicate", type=int, default=None)
+    parser.add_argument("-exp", "--exp_name", type=str, default=None)
     parser.add_argument(
         "-w", "--window", type=str, choices=["none", "per_exp", "per_well", "per_cell"]
     )
     parser.add_argument("-plot", "--plot", type=int, default=0, choices=[0, 1])
     args = parser.parse_args()
+    in_dir = args.in_dir[:-1] if args.in_dir[-1] == "/" else args.in_dir
+    out_dir = f"{in_dir}_gr"
 
-    # Set data type
-    data_type = label_data_type(args.in_dir)
+    if args.exp_name is None and args.plot == 0:
+        write_run_script(in_dir, out_dir, args.run_cmd)
+        return
 
-    # Check that input data path exists
-    if not os.path.exists(f"{args.data_dir}/{args.in_dir}"):
-        raise ValueError("Path to existing data does not exist.")
-
-    # Create new path for output data
-    save_loc = f"{args.data_dir}/{args.out_dir}"
-    if not os.path.exists(save_loc):
-        os.makedirs(save_loc)
-
-    # Save growth rate dataframes for each experiment
     if args.plot == 0:
-        for exp_name in os.listdir(f"{args.data_dir}/{args.in_dir}"):
-            if (
-                os.path.isfile(f"{args.data_dir}/{args.in_dir}/{exp_name}")
-                or exp_name == "layout_files"
-            ):
-                continue
-            print(exp_name)
-            save_growth_rate(
-                f"{args.data_dir}/{args.in_dir}",
-                save_loc,
-                exp_name,
-                args.window,
-            )
+        save_growth_rate(in_dir, out_dir, args.replicate, args.exp_name, args.window)
 
-    # Save results
     if args.plot == 1:
-        df = get_growth_rates(save_loc, f"{args.data_dir}/{args.in_dir}")
-
-        print_stats(df)
-
-        plot_dynamics(save_loc, df, data_type)
-
-        models = df["Model"].unique()
-        for model in models:
-            if model == "Ground Truth":
-                continue
-            df_model = df.copy()
-            if "Ground Truth" in models:
-                df_model.loc[df_model["Model"] == "Ground Truth", "Model"] = model
-                df = pd.concat(
-                    [df_model[df_model["Exponential Growth Window Strategy"] == "Ground Truth"], df]
-                )
-            plot_qualitative(
-                save_loc,
-                df_model[df_model["Model"] == model],
-                focal_col="Exponential Growth Window Strategy",
-                sub_col=model,
-            )
-
-        df = df[df["Model"] != "Ground Truth"]
-        plot_agreement(save_loc, df, data_type)
-
-        df = df[df["Exponential Growth Window Strategy"] != "Ground Truth"]
-        plot_parameter_ranges(save_loc, df, data_type)
-        plot_errors(
-            save_loc, df, sns.barplot, "Exponential Growth Window Strategy", "Error", "Model"
-        )
-        plot_errors(save_loc, df, sns.barplot, "Exponential Growth Window Strategy", "Error", None)
-        plot_errors(save_loc, df, sns.barplot, "Exponential Growth Window Strategy", "BIC", "Model")
-        plot_errors(save_loc, df, sns.barplot, "Exponential Growth Window Strategy", "BIC", None)
+        #df = get_growth_rates(in_dir, out_dir)
+        print("todo implement plots")
 
 
 if __name__ == "__main__":
